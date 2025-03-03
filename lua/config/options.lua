a6d46270916adc537a4249e1b1c6c4963a65141e
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-03 20:09:53 +0900

    feat: add support for clipboard under linux

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- needs to be set before lazy vim is loaded
+--
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- allows neovim to access system clipboard on linux
+vim.opt.clipboard = "unnamedplus"

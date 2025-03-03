ae20b9cf83c9a7be34350f037c380fd63672c13d
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-03 20:12:27 +0900

    feat: add vertical rulers at col 80 and 120

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,6 @@ vim.g.maplocalleader = "\\"
 
 -- allows neovim to access system clipboard on linux
 vim.opt.clipboard = "unnamedplus"
+
+-- vertical rulers at column 80 and 120
+vim.opt.colorcolumn = "80,100"

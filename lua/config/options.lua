3986ecb6d8ada5797a16c592c881191affa045b2
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-03 20:26:35 +0900

    feat: display trailing whitespaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,8 @@ vim.opt.clipboard = "unnamedplus"
 
 -- vertical rulers at column 80 and 100
 vim.opt.colorcolumn = "80,100"
+
+-- display trailing whitespaces
+vim.opt.list = true
+vim.opt.listchars:append('space:⋅')
+vim.opt.listchars:append('trail:⋅')

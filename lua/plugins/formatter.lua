98503d120e9f8fc5014fd8ba81dc791d187cd70f
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-04 19:19:57 +0900

    feat: trim trailing whitespaces and trailing empty newlines with conform

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,6 +5,7 @@ return {
       require('conform').setup({
         formatters_by_ft = {
           lua = { 'stylua' },
+          ['_'] = { 'trim_whitespace', 'trim_newlines' },
         },
         format_on_save = {
           timeout_ms = 2000,

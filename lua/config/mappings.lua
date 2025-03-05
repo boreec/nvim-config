1eef9cadcefecdc68faa3018a5b74a6db6084304
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-05 20:01:01 +0900

    feat: move telescope mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,11 +1,39 @@
+local telescope = require('telescope.builtin')
+
+-- Common options for mappings
+local opt = { noremap = true, silent = true }
+
+-- === General Mappings ===
+vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opt)
+vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opt)
+vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opt)
+vim.keymap.set('n', '<leader>fr', function()
+  vim.lsp.buf.format({ async = true })
+end, opt)
+
+-- === Telescope Mappings (Always Available) ===
+local telescope_mappings = {
+  ['<leader>ff'] = telescope.find_files,
+  ['<leader>fg'] = telescope.live_grep,
+  ['<leader>fb'] = telescope.buffers,
+}
+for key, action in pairs(telescope_mappings) do
+  vim.keymap.set('n', key, action, opt)
+end
+
+-- === LSP-Specific Mappings (Only When an LSP Attaches) ===
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
-    local opts = { noremap = true, silent = true, buffer = args.buf }
-    vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<leader>f', function()
-      vim.lsp.buf.format({ async = true })
-    end, opts)
+    local lsp_opts = { noremap = true, silent = true, buffer = args.buf }
+    local lsp_mappings = {
+      ['gd'] = telescope.lsp_definitions,
+      ['gi'] = telescope.lsp_implementations,
+      ['<leader>d'] = telescope.diagnostics,
+      ['<leader>s'] = telescope.lsp_document_symbols,
+      ['gr'] = telescope.lsp_references,
+    }
+    for key, action in pairs(lsp_mappings) do
+      vim.keymap.set('n', key, action, lsp_opts)
+    end
   end,
 })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,23 +4,12 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      local telescope = require('telescope')
-
-      -- Telescope setup
-      telescope.setup({
+      require('telescope').setup({
         defaults = {
           borderchars = { '═', '║', '═', '║', '╔', '╗', '╝', '╚' },
           file_ignore_patterns = { 'node_modules', '.git', 'vendor' },
         },
       })
-
-      -- Key mappings
-      local opt = { noremap = true, silent = true }
-      local builtin = require('telescope.builtin')
-
-      vim.keymap.set('n', '<Leader>ff', builtin.find_files, opt)
-      vim.keymap.set('n', '<Leader>fg', builtin.live_grep, opt)
-      vim.keymap.set('n', '<Leader>fb', builtin.buffers, opt)
     end,
   },
 }

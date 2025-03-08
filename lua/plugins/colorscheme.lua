dc348859d3a87790fa4712f9060084e676432234
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-08 16:34:21 +0900

    feat: set keywords and functions as bold style

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,16 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require('github-theme').setup({})
+      require('github-theme').setup({
+        options = {
+          styles = {
+            comments = 'NONE',
+            functions = 'bold',
+            keywords = 'bold',
+            types = 'bold',
+          },
+        },
+      })
       vim.cmd('colorscheme github_dark')
     end,
   },

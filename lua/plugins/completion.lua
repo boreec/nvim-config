5c638031aff72c3c42bf30d73a555fe44a5e1c44
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-08 09:15:06 +0900

    feat: show documentation for blink completions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,13 +1,22 @@
 return {
   {
     'saghen/blink.cmp',
-    version = 'v0.13.1',
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      draw = {
+        columns = {
+          { 'label', 'label_description', gap = 1 },
+          { 'kind_icon', 'kind' },
+        },
+      },
+    },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
     opts = {
       keymap = { preset = 'enter' },
     },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    version = 'v0.13.1',
   },
 }

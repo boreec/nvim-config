86bbae6a22b8ea562aa4ff3a099acb88c2ea9536
Cyprien Borée <cyprien.boree@tensorenergy.jp>
2025-03-10 10:02:50 +0900

    feat: add kubectl.nvim

---
+++ lua/plugins/kubernetes.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'ramilito/kubectl.nvim',
+    config = function()
+      require('kubectl').setup()
+    end,
+    opts = {},
+    cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
+    keys = {
+      { '<leader>K', '<cmd>lua require("kubectl").toggle()<cr>' },
+      { '<C-k>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
+      { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
+      { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
+      { '<C-t>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
+    },
+  },
+}

92df015c7aaabf9eb0c6e6440ef23b131280c782
Cyprien Boree <51238594+boreec@users.noreply.github.com>
2026-02-11 14:56:27 +0000

    chore: replace prettier with biome for html, json and markdown

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,16 +1,16 @@
 return {
   {
     'stevearc/conform.nvim',
-    version = 'v9.0.0',
+    version = 'v9.1.0',
     event = { 'BufWritePre' },
     config = function()
       require('conform').setup({
         formatters_by_ft = {
           go = { 'gofumpt', 'goimports' },
-          html = { 'prettier' },
-          json = { 'prettier' },
+          html = { 'biome' },
+          json = { 'biome' },
           lua = { 'stylua' },
-          markdown = { 'prettier' },
+          markdown = { 'biome' },
           rust = { 'rustfmt' },
           toml = { 'taplo' },
           typescript = { 'biome' },

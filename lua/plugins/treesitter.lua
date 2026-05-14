68a1889c5b833054d55e6f89a9f5b8a51687933e
Cyprien Borée <cyprien.boree@tensorenergy.jp>
2026-05-14 09:00:07 +0900

    claude: turn checkpoint

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,60 +1,56 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
-    -- version = 'v10.0.0',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        additional_vim_regex_highlighting = false,
-        -- The listed parsers MUST always be installed
-        ensure_installed = {
-          'bash',
-          'c',
-          'cpp',
-          'csv',
-          'dockerfile',
-          'gitignore',
-          'go',
-          'gomod',
-          'gosum',
-          'gotmpl',
-          'gowork',
-          'graphql',
-          'hcl',
-          'json',
-          'lua',
-          'make',
-          'markdown',
-          'markdown_inline',
-          'python',
-          'proto',
-          'rust',
-          'sql',
-          'terraform',
-          'toml',
-          'typescript',
-          'xml',
-          'yaml',
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-        },
+    init = function()
+      local ensure_installed = {
+        'bash',
+        'c',
+        'cpp',
+        'csv',
+        'dockerfile',
+        'gitignore',
+        'go',
+        'gomod',
+        'gosum',
+        'gotmpl',
+        'gowork',
+        'graphql',
+        'hcl',
+        'json',
+        'lua',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'proto',
+        'rust',
+        'sql',
+        'terraform',
+        'toml',
+        'typescript',
+        'xml',
+        'yaml',
+      }
+      local installed = require('nvim-treesitter.config').get_installed()
+      local to_install = vim.iter(ensure_installed)
+        :filter(function(p)
+          return not vim.tbl_contains(installed, p)
+        end)
+        :totable()
+      if #to_install > 0 then
+        require('nvim-treesitter').install(to_install)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
-      -- Remove the undercul for markdown links
+
       vim.api.nvim_set_hl(0, '@markup.link.url.markdown_inline', { undercurl = false })
     end,
     dependencies = {
@@ -69,7 +65,31 @@ return {
           })
         end,
       },
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
+        config = function()
+          require('nvim-treesitter-textobjects').setup({
+            select = {
+              lookahead = true,
+            },
+          })
+
+          local select = require('nvim-treesitter-textobjects.select')
+          vim.keymap.set({ 'x', 'o' }, 'af', function()
+            select.select_textobject('@function.outer', 'textobjects')
+          end)
+          vim.keymap.set({ 'x', 'o' }, 'if', function()
+            select.select_textobject('@function.inner', 'textobjects')
+          end)
+          vim.keymap.set({ 'x', 'o' }, 'ac', function()
+            select.select_textobject('@class.outer', 'textobjects')
+          end)
+          vim.keymap.set({ 'x', 'o' }, 'ic', function()
+            select.select_textobject('@class.inner', 'textobjects')
+          end)
+        end,
+      },
     },
   },
 }
